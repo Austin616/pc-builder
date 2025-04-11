bfcccab6cd138c1f214a3b9d72c2d4c219775024
Austin Tran <austintran616@gmail.com>
2025-04-10 21:18:03 -0500

    some stuff

---
+++ src/app/components/SliderComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SliderComponent = () => {
+  return (
+    <div>
+      This is the slider
+    </div>
+  )
+}
+
+export default SliderComponent
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-'use client';
+"use client";
 
 import React from "react";
-import Navbar from "./components/Navbar";
+import Landing from "./landing/page";
+import "./globals.css";
 
 export default function Home() {
   return (
-    <div className="">
-      <Navbar />
+    <div className="bg-desktop-dark dark:bg-desktop-light bg-cover bg-no-repeat bg-center min-h-screen w-full flex items-center justify-center">
+      <Landing />
     </div>
   );
 }
